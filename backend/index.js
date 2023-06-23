e9f73817a680fd7c42a0fa0c9c0ee9029713670f
deepak <d1620k@yahoo.com>
2023-06-23 17:47:51 +0530

    add changes for backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,28 @@
-const express = require("express")
-const app = express();
-require('./db/config')
-const Order = require('./db/order')
-app.use(express.json())
-app.post("/register",async (req,res)=>{
-    console.log(req)
-    const addOrder = new Order(req.body)
-    let result = await addOrder.save()
-    res.send(result)
-})
 
-app.listen("5000")
+// add express for get and post api
+const express = require("express");
+const app = express();
+
+// Import config.js file for db connection
+require("./db/config");
+
+// import order schema for save data in db
+const Order = require("./db/order");
+
+// get request body in json format
+app.use(express.json());
+
+//root api call at localhost:5000
+app.get("/", async (req, res) => {
+  res.send("hellow world");
+});
+
+// api call from new Order 
+app.post("/createOrder", async (req, res) => {
+  console.log(req);
+  const addOrder = new Order(req.body);
+  let result = await addOrder.save();
+  res.send(result);
+});
+
+app.listen("5000");
