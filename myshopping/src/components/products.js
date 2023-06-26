50ba016290cac6b51ef55ceb46139dfffaec9d2c
deepak <d1620k@yahoo.com>
2023-06-26 18:39:09 +0530

    changes in done

--- myshopping/src/components/products.js
+++ myshopping/src/components/products.js
@@ -1,38 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../store/product";
 import { useEffect } from "react";
-// import {
-//   MDBBtn,
-//   MDBCard,
-//   MDBCardBody,
-//   MDBCardImage,
-//   MDBCol,
-//   MDBContainer,
-//   MDBIcon,
-//   // MDBInput,
-//   MDBRow,
-//   MDBTypography,
-// } from "mdb-react-ui-kit";
 import Header from "./Header/header";
 import AllProductLists from "./allproducts";
 
+// productList function based component 
 const ProductList = () => {
-console.log("first")
+  // use dispatch as fetching product thru generating fetchProducts action
   const dispatch = useDispatch(fetchProducts);
+
+  //set the value of products from state object
   const product = useSelector((state) => state.product.productlist.products);
   
   useEffect(() => {
     async function fetchData() {
-     await dispatch((fetchProducts()));
+      await dispatch(fetchProducts());
     }
- fetchData()
+    fetchData();
   }, [dispatch]);
 
-console.log("direct",product)
   return (
     <>
       <Header />
- <AllProductLists product={product}/>
+      <AllProductLists product={product} />
     </>
   );
 };
--- myshopping/src/store/product.js
+++ myshopping/src/store/product.js
@@ -1,10 +1,12 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 
+// Call Api thru fetch and get data into json format thru use json()
 export const fetchProducts = createAsyncThunk("fetchProducts", async ()=>{
     const response = await fetch("https://dummyjson.com/products");
     return response.json();
 })
 
+// slice is create for intial data setup and make specific object for storing data and use reducers for performing actions
 const productSlice  = createSlice({
     name: "product",
     initialState: {
@@ -12,36 +14,23 @@ const productSlice  = createSlice({
         loading: false,
     },
     extraReducers:(builder)=>{
+        // When request is in pending state and loding will be true during pending state
         builder.addCase(fetchProducts.pending, (state,action)=>{
             state.loading = true;
         });
+
+        // request is fulfilled and loding will true and data will store in state object
         builder.addCase(fetchProducts.fulfilled, (state,action)=>{
             state.loading = false;
             state.productlist = action.payload;
         });
+
+        // Request rejected and error will occur
         builder.addCase(fetchProducts.rejected, (state,action)=>{
             console.log("error- Occur", action.payload)
             state.isError = true;
         })
     }
-    
-    // reducers: {
-    //     productRequested: (product, action) => {
-    //         console.log(product,"productRequested")
-    //         product.loading = true;
-    //     },
-
-    //     productReceived: (product, action) => {
-    //         console.log(product,"productReceived")
-    //         product.productlist = action.payload;
-    //         product.loading = false;
-    //     },
-
-    //     productRequestFailed: (product, action) => {
-    //         console.log(product,"productRequestFailed")
-    //         product.loading = false;
-    //     },
-    // },
 });
 
 export default productSlice.reducer;
